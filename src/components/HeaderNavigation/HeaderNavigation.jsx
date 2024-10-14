75c613c847d9a738b2ed825bfa5c74c5773a7f5c
Evgeniy <evgeniy6183@gmail.com>
2024-10-14 15:12:22 +0300

    up_main

--- src/components/HeaderNavigation/HeaderNavigation.jsx
+++ src/components/HeaderNavigation/HeaderNavigation.jsx
@@ -1,13 +1,16 @@
 import { Link, NavLink } from 'react-router-dom';
 
+// import css from './HeaderNavigation.module.css';
+
 export const HeaderNavigation = () => {
   return (
     <header>
-      <Link></Link>
+      <Link to="/">
+        <img src="" alt="" />
+      </Link>
       <nav>
-        <NavLink></NavLink>
-        <NavLink></NavLink>
-        <NavLink></NavLink>
+        <NavLink to="/">Sign up</NavLink>
+        <NavLink to="/">Sign in</NavLink>
       </nav>
     </header>
   );
