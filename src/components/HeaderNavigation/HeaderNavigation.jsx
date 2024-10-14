d3496f190992079d123f37d5d52a69459eabb5c6
Evgeniy <evgeniy6183@gmail.com>
2024-10-14 16:37:32 +0300

    up_header

--- src/components/HeaderNavigation/HeaderNavigation.jsx
+++ src/components/HeaderNavigation/HeaderNavigation.jsx
@@ -1,16 +1,26 @@
 import { Link, NavLink } from 'react-router-dom';
 
-// import css from './HeaderNavigation.module.css';
+import css from './HeaderNavigation.module.css';
+import clsx from 'clsx';
 
 export const HeaderNavigation = () => {
+  const getNavLinkClassName = ({ isActive }) =>
+    clsx(css.linkText, {
+      [css.active]: isActive,
+    });
+
   return (
-    <header>
+    <header className={css.headerContainer}>
       <Link to="/">
-        <img src="" alt="" />
+        <img className={css.headerLogo} src="" alt="" />
       </Link>
-      <nav>
-        <NavLink to="/">Sign up</NavLink>
-        <NavLink to="/">Sign in</NavLink>
+      <nav className={css.navLink}>
+        <NavLink to="/" className={getNavLinkClassName}>
+          Sign up
+        </NavLink>
+        <NavLink to="/" className={getNavLinkClassName}>
+          Sign in
+        </NavLink>
       </nav>
     </header>
   );
